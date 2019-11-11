33db940ab929f02ee3ff43492452f7c91b06ec6d
mis321396 <mis321396@gmail.com>
2019-11-11 15:35:24 +0800

    建立專案

---
+++ src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+    	//我要帶的資料
+        model.addAttribute("text", "Hello World!!");
+        //要回傳的網頁
+        return "hello"; // resources/hello.html
+    }
+}
