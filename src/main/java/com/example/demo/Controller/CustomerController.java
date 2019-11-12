20341b7af0c940a430a04871269e0fd7c78a50e6
Kesslarr <emily10616@gmail.com>
2019-11-12 14:20:51 +0800

    增加session存取帳號 修改Entity OrderDetail(實作序列)  新增登出功能

--- src/main/java/com/example/demo/Controller/CustomerController.java
+++ src/main/java/com/example/demo/Controller/CustomerController.java
@@ -2,6 +2,9 @@ package com.example.demo.Controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +23,10 @@ public class CustomerController {
 	
 	//呈現登入頁面
 	@GetMapping("/login")
-	public String loginPage(Model model) {
+	public String loginPage(Model model, HttpServletRequest request) {
+		if(request.getAttribute("msg") != null && !"".equals(request.getAttribute("msg"))) {
+			model.addAttribute("msg", request.getAttribute("msg").toString());
+		}
 		List<Customer> all = customerRepository.findAll();
 		model.addAttribute("cus", all);
 		return "login";
@@ -28,11 +34,23 @@ public class CustomerController {
 	
 	//檢核是否登入成功
 	@PostMapping("/checkLogin")
-	public String checkLogin(Model model, @RequestParam("account") String account, @RequestParam("password") String password) {
+	public String checkLogin(Model model, @RequestParam("account") String account, @RequestParam("password") String password, HttpSession session, HttpServletRequest request) {
 		Customer check = customerRepository.checkLogin(account, password);
 		if(check != null) {
+			session.setAttribute("user", account);
 			return "hello";
+		}else {
+			request.setAttribute("msg", "帳號或密碼輸入錯誤");
 		}
 		return "login";
+	}
+	
+	@PostMapping("/logout")
+	public String logout(HttpSession session) {
+		if(session != null) {
+			session.removeAttribute("user");
+		}
+		
+		return "redirect:/login";
 	}
 }
--- src/main/java/com/example/demo/entity/OrderDetail.java
+++ src/main/java/com/example/demo/entity/OrderDetail.java
@@ -1,12 +1,16 @@
 package com.example.demo.entity;
 
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name="OrderDetail")
-public class OrderDetail {
+public class OrderDetail implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@Column (name = "OID")
